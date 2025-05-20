7905e3cb9232dc703662b2e1258d7278170d796a
cbwheels18@gmail.com <Cody@settersuite.com>
2025-05-19 20:01:34 -0600

    learning api with open weather

---
+++ scripts/openweather.js
@@ -0,0 +1,34 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const apiKey = '803a5617f399eff11d7cd9234a030f60';
+const lat = '49.75056';
+const lon = '6.63569';
+const units = 'imperial';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch (url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    console.log('hello')
+    captionDesc.innerHTML = data.weather[0].description;
+    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)} &deg;F</strong>`
+    weatherIcon.setAttribute('src', `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+}
+
+
+apiFetch();
